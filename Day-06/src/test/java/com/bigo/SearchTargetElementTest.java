08a5eb5c435a6f48233038bf07e1d44d7e44a4b0
kritz999 <kritikabdas007@gmail.com>
2025-02-07 13:38:14 +0530

    [Kritika B.Das] add: Day-06 started

---
+++ Day-06/src/test/java/com/bigo/SearchTargetElementTest.java
@@ -0,0 +1,14 @@
+package com.bigo;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchTargetElementTest {
+
+    @Test
+            SearchTargetElement
+    int [] DataSet ={1000,10000,1000000};
+
+
+}
