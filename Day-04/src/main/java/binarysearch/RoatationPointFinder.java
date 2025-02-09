dd0710b6f622aa4a567b31e468fdecc49aedbc62
kritz999 <kritikabdas007@gmail.com>
2025-02-09 21:49:35 +0530

    [Kritika B.Das] add: rotataion point finder

---
+++ Day-04/src/main/java/binarysearch/RoatationPointFinder.java
@@ -0,0 +1,28 @@
+package binarysearch;
+
+public class RoatationPointFinder {
+    public static int findRotationPoint(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+        int rotationIndex = findRotationPoint(arr);
+        System.out.println("Rotation Point Index: " + rotationIndex);
+        System.out.println("Smallest Element: " + arr[rotationIndex]);
+    }
+
+
+}
